35371f61dba2f801660ccf7a36875f42e5dfbaf8
javacefu <javier.acevedof@gmail.com>
2025-07-25 18:31:48 -0400

    Actualizacion

--- src/Tabla.java
+++ src/Tabla.java
@@ -11,14 +11,15 @@ public class Tabla {
         System.out.println("Codigos de monedas disponibles:");
 
         System.out.println("Mostrando tabla sin procesar: " + data.getMonedaYValor().keySet());
-        System.out.printf("%-10s |","MONEDA");
+        System.out.printf("%-10s |", "MONEDA");
         System.out.println("-----------------");
 
+        data.getMonedaYValor().keySet();
 
-        data.getMonedaYValor()
-                .entrySet()
-                .stream()
-                .forEach(stringDoubleEntry -> System.out.println(stringDoubleEntry.getKey() + " : " + stringDoubleEntry.getValue()));
+//        for (int i = 0; i < data.getMonedaYValor().size(); i++) {
+//            System.out.println(data.getMonedaYValor().get());
+//        }
+
 
         System.out.println("Volviendo al menu principal");
         Menu.mostrarMenu();
